0c6de6cc72f195b8fbbbb2173c5cf16a2661f56a
Brinda Gupta <55502062+spectacles9468@users.noreply.github.com>
2025-02-27 14:46:50 +0530

    Update pdf_processing.py

--- pdf_processing.py
+++ pdf_processing.py
@@ -1,15 +1,13 @@
-from pypdf import PdfReader
+import pymupdf
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 # Extracting text from pdf
 def extract_text_from_pdf(pdf_path):
-    reader = PdfReader(pdf_path)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
+    doc = pymupdf.open(pdf_path)
+    text = chr(12).join([page.get_text("text") for page in doc])
     return text
 
 # Chunking the text for the model
-def chunk_text(text, chunk_size=500, chunk_overlap=100):
+def chunk_text(text, chunk_size=1000, chunk_overlap=100):
     splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     return splitter.split_text(text)
